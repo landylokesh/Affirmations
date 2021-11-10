21aebd7726ff1c574a18c6a4fddf1e7af8aa6c51
landylokesh <lokesh.valayapathi@zohocorp.com>
2021-11-10 15:31:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset:  List<Affirmation>
+    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        //Create a new view
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.textView.text = context.resources.getString(dataset[position].stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+
+}
